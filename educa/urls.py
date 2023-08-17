15331ba0b490d12dc88083f0c07cac3a1e85262c
Julia Chuprova <nikaniks57@gmail.com>
2023-08-17 19:18:47 +0300

    add models and login/logout pages

--- educa/urls.py
+++ educa/urls.py
@@ -1,9 +1,7 @@
-
-"""
-URL configuration for educa project.
+"""educa URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -17,7 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path('accounts/login/', auth_views.LoginView.as_view(),
+         name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(),
+         name='logout'),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
